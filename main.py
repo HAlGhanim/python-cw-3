8f89dfdc7551883627bf4f4a0274ef36cad30b26
HAlGhanim <imzaji98@gmail.com>
2022-12-04 18:01:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # الجزء الاول
 favorite_animals = ['dog','cat','monkey','rabbit']  
-# print(favorite_animals)
-# print(favorite_animals[1])
-# favorite_animals.remove('monkey')
-# print(favorite_animals)
+print(favorite_animals)
+print(favorite_animals[1])
+favorite_animals.remove('monkey')
+print(favorite_animals)
 
 # الجزء الثاني
-# favorite_animals.append('chimpanzee')
-# print(favorite_animals)
-# for fav in favorite_animals:
-#     print(f"I love {fav}")
+favorite_animals.append('chimpanzee')
+print(favorite_animals)
+for fav in favorite_animals:
+    print(f"I love {fav}")
 
 # الجزء الثالث
 numbers =[5, 4, 3, 2, 1]
@@ -17,3 +17,4 @@ numbers_sum = 0
 for numbers in numbers:
     numbers_sum += numbers
 print(numbers_sum)
+
